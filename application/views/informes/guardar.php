a308cb4de78c92843511ec80560de17103df5575
Fernando Gaitán <fernandoggaitan@gmail.com>
2014-08-10 04:36:33 -0300

    Update guardar.php

--- application/views/informes/guardar.php
+++ application/views/informes/guardar.php
@@ -1,16 +1,16 @@
 <h1> Guardar informe </h1>
-<form method="post" action="<?php echo base_url() ?>informes/guardar_post/<?php echo ($informe) ? $informe->id : null ?>">
+<form method="post" action="<?php echo base_url() ?>informes/guardar_post/<?php echo $id ?>">
     <div class="row">
         <label> Título </label>
-        <input type="text" name="titulo" required="required" value="<?php echo ($informe) ? $informe->titulo : null; ?>" />
+        <input type="text" name="titulo" required="required" value="<?php echo $titulo ?>" />
     </div>
     <div class="row">
         <label> Descripción </label>
-        <textarea name="descripcion" cols="100" rows="10" required="required" style="width: 100%;"><?php echo ($informe) ? $informe->descripcion : null; ?></textarea>
+        <textarea name="descripcion" cols="100" rows="10" required="required" style="width: 100%;"><?php echo $descripcion; ?></textarea>
     </div>
     <div class="row">
         <label> Prioridad </label>
-        <input type="number" min="1" max="5" name="prioridad" required="required" value="<?php echo ($informe) ? $informe->prioridad : null; ?>" />
+        <input type="number" min="1" max="5" name="prioridad" required="required" value="<?php echo $prioridad; ?>" />
     </div>
     <div class="row">
         <input type="submit" class="btn btn-success" value="Guardar" />
